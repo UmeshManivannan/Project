43977ab594d9f6695719a7328c088263ed0f7b82
UmeshManivannan <umeshthalapathy10@gmail.com>
2024-07-18 03:05:12 +0530

    php added to the form, only submit is successfull

---
+++ index.php
@@ -0,0 +1,46 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CrudOperation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+
+</head>
+<body>
+      <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card" style="width: 58rem;">
+                <div class="card-header">
+                    <h1>Customer Details</h1>
+                </div>
+                    <div class="card-body">
+                    <button class="btn btn-success"> <a href = "add.php" class="text-light">Add new</a></button>
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">FirstName</th>
+                                    <th scope="col">LastName</th>
+                                    <th scope="col">Address</th>
+                                    <th scope="col">Mobile</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                   
+                                </tbody>
+                            </table>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+</body>
+</html>
