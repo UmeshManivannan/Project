1d4c328edc0e165769b4bb604c8a13d95e290a8f
UmeshManivannan <umeshthalapathy10@gmail.com>
2024-08-01 02:46:31 +0530

    last page and footer created only the ui part

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-<script>
-let scrollcontainer = document.querySelector(".all-brand");
-let backbtn = document.getElementById("backbtn");
-let nextbtn = document.getElementById("nextbtn");
 
-scrollcontainer.addEventListener("wheel", (evt)=>{
-    evt.preventDefault();
-    scrollcontainer.scrollLeft += evt.deltaY;
-});
-</script>
